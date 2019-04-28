3a9573162540e32d97c056276ca822171344c15d
Carlos Tiago <carlostiagodesousa@gmail.com>
2019-04-28 14:42:07 -0300

    deploy para heroku

--- src/main/java/br/com/siscomanda/config/jpa/EntityManagerProducer.java
+++ src/main/java/br/com/siscomanda/config/jpa/EntityManagerProducer.java
@@ -4,6 +4,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -12,9 +13,12 @@ import javax.persistence.Persistence;
 public class EntityManagerProducer {
 
 	private EntityManagerFactory factory;
-
+	
+	@Inject
+	private PersistenceProperties properties;
+	
 	public EntityManagerProducer() {
-		this.factory = Persistence.createEntityManagerFactory("siscomandaPU");
+		this.factory = Persistence.createEntityManagerFactory("siscomandaPU", properties.get());
 	}
 
 	@Produces
